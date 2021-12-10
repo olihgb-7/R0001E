a00de1814c25b03f5c962c805fa2e4e6efac5e2c
olihgb-7 <olihgb-7@student.ltu.se>
2021-12-11 00:22:48 +0100

    Lab3 PID-Simulink

--- Lab3/Lab3.m
+++ Lab3/Lab3.m
@@ -15,4 +15,7 @@ GR = GPID*GF;
 %% Kretsöverföringsfunktionen
 GK = GR*GP;
 
-GTOT = (GPI*GP)/(1+GPI*GP*GF^2+GP*GD*GF);
+GTOT = (GPI*GP)/(1+GPI*GP*GF*GF+GP*GD*GF);
+
+H = [2.5 5.4 6.9 8.2 9.6 13.1 15.5];  
+Y = [0.10 2.33 3.43 4.49 5.54 8.22 9.91];
--- Lab3/PIDdesign.m
+++ Lab3/PIDdesign.m
@@ -7,9 +7,9 @@
 % * Fasvridning i wc (phiGPwc [rad])
 
 %% Designparametrar
-wc = 0.5;
-Phim = 62*(pi/180);
-K8 = 100;
+wc = 0.09;
+Phim = 65*(pi/180);
+K8 = 4.5;
 
 %% Processparametrar
 K1 = 5.41;
@@ -23,8 +23,8 @@ G12 = tf([0 K12],[T2 1]);
 GP = G1*G12;
 
 %% Amplitudfunktion
-absG1wc = abs(K1/(T1*wc*i + 1));
-absG12wc = abs(K12/(T2*wc*i + 1));
+absG1wc = K1/sqrt(1+(T1*wc)^2);     %%abs(K1/(T1*wc*i + 1))
+absG12wc = K12/sqrt(1+(T2*wc)^2);   %%abs(K12/(T2*wc*i + 1))
 absGPwc = absG1wc*absG12wc;
 
 %% Fasfunktion
@@ -36,8 +36,7 @@ phiGPwc = phiG1wc+phiG12wc;
 PhiR=-phiGPwc-pi+Phim;
 my=K8*absGPwc;
 Storre_an_1=my*cos(PhiR)
-
-%% Regulatorparametrar
+% Regulatorparametrar
 Td=(sqrt(my*(my-cos(PhiR)))+my*sin(PhiR))/(2*wc*(my*cos(PhiR)-1))
 Tf=Td*cos(PhiR)/(my+wc*Td*sin(PhiR))
 Ti=4*Td
